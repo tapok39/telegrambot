24d0affd55d41da1600a67202de4e1f6393bc8ef
Anton Palapin <aantongarmon@gmail.com>
2022-08-26 15:55:05 +0300

    123

---
+++ 123.py
@@ -0,0 +1,6 @@
+import operator
+d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+result = dict(sorted(d.items(), key=operator.itemgetter(1)))
+result2 = dict(sorted(d.items(), key=operator.itemgetter(1), reverse=True))
+print(result, result2)
+
